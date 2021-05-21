a1ff7641f4ca7d571fc6dbd0a5002bccd7e1d5e9
_dave <43000760+DaveLogs@users.noreply.github.com>
2021-05-21 16:28:36 +0900

    Add files via upload
    Initial commit

---
+++ convert.py
@@ -0,0 +1,116 @@
+"""
+
+Convert TextRecognitionDataGenerator's output data to Deep-Text-Recognition-Benchmark's input data.
+
+## Reference projects
+1. TextRecognitionDataGenerator:
+    https://github.com/Belval/TextRecognitionDataGenerator
+2. Deep-Text-Recognition-Benchmark:
+    https://github.com/clovaai/deep-text-recognition-benchmark
+
+## Example usage:
+    python convert.py \
+        --input_path ./input \
+        --output_path ./output
+
+## Data structure:
+    ./input
+    #   [gt]_[idx].[ext] - for TextRecognitionDataGenerator
+        a_001.jpg
+        b_002.jpg
+        ...
+
+    ./output
+        /images
+        #   image_[idx].[ext] - for Deep-Text-Recognition-Benchmark
+            image_001.jpg
+            image_002.jpg
+            ...
+        gt.txt
+
+## Ground truth file structure:
+#   gt.txt
+#   [filename]    \t      [gt]
+    images/image_001.jpg    a
+    images/image_002.jpg    b
+    ...
+
+"""
+
+import os
+import shutil
+import argparse
+
+
+def run(input_path, output_path):
+    print('input path: ', os.path.abspath(input_path))
+    print('output path: ', os.path.abspath(output_path))
+
+    if not os.path.exists(input_path):
+        print('\nInput data path [%s] is not found.\n' % os.path.abspath(input_path))
+        return
+
+    if os.path.exists(output_path):
+        print('\nOutput folder already exists.')
+        print('\nSo, delete all data of output folder [%s]\n' % os.path.abspath(output_path))
+        shutil.rmtree(output_path)
+
+    image_folder_name = 'images'
+    image_path = os.path.join(output_path, image_folder_name)
+    for path in [output_path, image_path]:
+        os.makedirs(path)
+
+    # Load input data
+    files, count = get_files(input_path)
+    digits = len(str(count))
+    print('Total file count: ', count)
+
+    # Create gt.txt and copy images
+    gt_file = open(os.path.join(output_path, 'gt.txt'), 'w', encoding='UTF8')
+    for idx, item in enumerate(files):
+        if (idx+1) % 100 == 0:
+            print(('\r%{}d / %{}d Processing !!'.format(digits, digits)) % (idx+1, len(files)), end='')
+
+        gt = os.path.basename(item).split('_')[0]   # remove index
+        ext = os.path.splitext(item)[1]
+
+        filename = os.path.join(image_folder_name, ('image_%0{}d'.format(digits) % idx) + ext)
+        gt_file.write('%s\t%s\n' % (filename, gt))
+        shutil.copy(item, os.path.join(output_path, filename))
+
+    gt_file.close()
+
+    print('\nConversion complete!\n')
+
+    return
+
+
+def get_files(path):
+    file_list = []
+
+    files = [f for f in os.listdir(path) if not f.startswith('.')]  # skip hidden file
+    abspath = os.path.abspath(path)
+    for file in files:
+        file_path = os.path.join(abspath, file)
+        file_list.append(file_path)
+
+    return file_list, len(file_list)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Convert TextRecognitionDataGenerator to Deep-Text-Recognition-Benchmark')
+
+    parser.add_argument('--input_path', dest='input_path', help='TextRecognitionDataGenerator data path')
+    parser.add_argument('--output_path', dest='output_path', help='Output path')
+
+    parsed_args = parser.parse_args()
+    return parsed_args
+
+
+if __name__ == '__main__':
+    # args = parse_arguments()
+    # run(input_path=args.input_path, output_path=args.output_path)
+
+    # for test
+    run(input_path='./input', output_path='./output')
+
